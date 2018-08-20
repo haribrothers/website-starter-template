9fc0a8ecdeb11ed76f8ccde6580f70244ef88c09
haribrothers <>
2018-08-20 10:58:53 +0530

    initial commit with webpack(v4) and babel

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+module.exports = {
+    entry: __dirname + "/src/app.js",
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader",
+                    options: {
+                        minimize: true
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
